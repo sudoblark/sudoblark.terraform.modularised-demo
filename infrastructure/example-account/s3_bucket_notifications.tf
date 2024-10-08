debfd37ccb8057b57b23d9a3610e81f8b8aa88cd
Benjamin Clark <bclark@sudoblark.com>
2024-10-08 17:46:25 +0100

    Create unzip bucket notification

--- infrastructure/example-account/s3_bucket_notifications.tf
+++ infrastructure/example-account/s3_bucket_notifications.tf
@@ -1,6 +1,9 @@
 module "s3_bucket_notifications" {
   source = "../modules/s3_bucket_notifications"
 
+  environment      = var.environment
+  application_name = var.application_name
+
   depends_on = [
     module.s3_bucket,
     module.lambda
---
+++ infrastructure/modules/s3_bucket_notifications/data.tf
@@ -0,0 +1,17 @@
+locals {
+  known_buckets = {
+    raw : {
+      name : lower("${var.environment}-${var.application_name}-raw")
+      arn : lower("arn:aws:s3:::fom-${var.environment}-${var.application_name}-raw")
+    },
+  }
+  known_lambdas = {
+    "unzip" : lower("aws-${var.environment}-${var.application_name}-unzip-lambda"),
+  }
+}
+
+# Lookup known lambdas for easy reference across stack
+data "aws_lambda_function" "known_lambdas" {
+  for_each      = local.known_lambdas
+  function_name = each.value
+}
