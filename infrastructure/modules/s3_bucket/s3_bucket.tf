732dad692924f26fb24dd12b3858affd025b1e0f
Benjamin Clark <bclark@sudoblark.com>
2024-10-08 12:46:15 +0100

    Version bump for s3_bucket module

--- infrastructure/modules/s3_bucket/s3_bucket.tf
+++ infrastructure/modules/s3_bucket/s3_bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source           = "github.com/sudoblark/sudoblark.terraform.module.aws.s3_bucket?ref=1.0.0"
+  source           = "github.com/sudoblark/sudoblark.terraform.module.aws.s3_bucket?ref=1.0.1"
   application_name = var.application_name
   environment      = var.environment
   raw_s3_buckets   = local.raw_s3_buckets
