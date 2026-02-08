3e5b0921f3b6ba5e100b2b8559a9428894b2e788
Benjamin Clark <bclark@sudoblark.com>
2026-02-08 17:59:59 +0000

    Add missing lambda permissions resource

--- modules/infrastructure/s3-notifications/notifications.tf
+++ modules/infrastructure/s3-notifications/notifications.tf
@@ -1,3 +1,13 @@
+resource "aws_lambda_permission" "allow_s3_invoke" {
+  for_each = { for idx, notif in var.lambda_notifications : idx => notif }
+
+  statement_id  = "AllowExecutionFromS3-${var.bucket_id}-${each.key}"
+  action        = "lambda:InvokeFunction"
+  function_name = each.value.lambda_function_arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.bucket_id}"
+}
+
 resource "aws_s3_bucket_notification" "notification" {
   bucket = var.bucket_id
 
@@ -11,4 +21,6 @@ resource "aws_s3_bucket_notification" "notification" {
       filter_suffix       = lambda_function.value.filter_suffix
     }
   }
+
+  depends_on = [aws_lambda_permission.allow_s3_invoke]
 }
